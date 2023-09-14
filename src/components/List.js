5758a17e83721b0790f0291bd764f182b42e4da2
KangYunJi <dbswl701@ajou.ac.kr>
2023-09-15 01:55:45 +0900

    design: list css 추가

--- src/components/List.js
+++ src/components/List.js
@@ -1,13 +1,24 @@
 import React from 'react'
+import deleteIcon from '../assets/icons/delete.svg';
+import writeIcon from '../assets/icons/write.svg';
 
 const List = ({data, handleDelete, handleModify}) => {
+  const style = {
+    width: '40px',
+    backgroundColor: 'transparent',
+    border: 'none'
+  }
   return (
-    <div style={{display: 'flex', flexDirection: 'row'}}>
+    <div style={{display: 'flex', flexDirection: 'row', border: '1px solid #d7d7d7', justifyContent: 'space-between', padding: '0px 20px'}}>
       <p>{data.title}</p>
       <p>{data.price}</p>
-      <div>
-        <button onClick={() => handleDelete(data.id)}>x</button>
-        <button onClick={() => handleModify(data)}>o</button>
+      <div style={{display: 'flex'}}>
+        <button onClick={() => handleModify(data)} style={style}>
+          <img src={writeIcon} style={{ width: '100%' }} />
+        </button>
+        <button onClick={() => handleDelete(data.id)} style={style}>
+          <img src={deleteIcon} style={{ width: '100%' }} />
+        </button>
       </div>
     </div>
   )
--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,13 +1,34 @@
 import React from 'react'
 import List from './List';
+import deleteIcon from '../assets/icons/delete.svg';
 
 const Lists = ({datas, handleClear, handleDelete, handleModify}) => {
+  const style = {
+    button: {
+      backgroundColor: 'rgb(142,169,89)',
+      border: 'none',
+      color: 'white',
+      width: '130px',
+      height: '30px',
+      borderRadius: '2px',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-evenly'
+    },
+    icon: {
+      width: '25px',
+      fill: 'white',
+      color: 'white',
+    }
+    // rgb(197, 81, 80)
+  }
   return (
-    <div style={{display: 'flex', flexDirection: 'column'}}>
+    <div style={{display: 'flex', flexDirection: 'column', marginTop: '40px', gap: '20px'}}>
       {datas.map((data) => (
         <List key={data.id} data={data} handleDelete={handleDelete} handleModify={handleModify} />
       ))}
-      <button onClick={handleClear}>목록 지우기</button>
+      <button style={style.button} onClick={handleClear}>목록 지우기
+      <img src={deleteIcon} style={style.icon} fill="white" /></button>
     </div>
   )
 }
